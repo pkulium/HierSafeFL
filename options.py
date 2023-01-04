6157a5678bc1466d0d8011f62fb79007a9e57c49
pkulium <54947136+pkulium@users.noreply.github.com>
2023-01-03 22:55:19 -0600

    Update options.py

--- options.py
+++ options.py
@@ -174,6 +174,19 @@ def args_parser():
         help = 'number of all honest clients'
     )
 
+    parser.add_argument(
+        '--attack',
+        type = str,
+        default = 'target',
+        help = 'type of attack'
+    )
+
+    parser.add_argument(
+        '--num_reference',
+        type = int,
+        default = '50',
+        help = 'type of attack'
+    )
 
     args = parser.parse_args()
     args.cuda = torch.cuda.is_available()
