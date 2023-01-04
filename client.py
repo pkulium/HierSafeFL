a37214055fffa69dbe4e5ec81807beccb3192c6c
pkulium <54947136+pkulium@users.noreply.github.com>
2023-01-03 22:54:20 -0600

    Update client.py

--- client.py
+++ client.py
@@ -26,7 +26,10 @@ class Client():
         self.honest = honest
         self.grad_history = 0
         self.device = device
-        self.dataset = args.dataset
+        self.training_model = args.model
+        self.attack = args.attack
+        self.s_prime = None
+        self.receipt = None
 
     def local_update(self, num_iter, device):
         itered_num = 0
@@ -34,12 +37,14 @@ class Client():
         end = False
         self.grad_history = 0
         # the upperbound selected in the following is because it is expected that one local update will never reach 1000
-        iter = 1
-        for epoch in range(iter):
+        for epoch in range(1000):
             for data in self.train_loader:
                 inputs, labels = data
                 if not self.honest:
-                    labels.apply_(lambda x: 7)
+                    if self.attack == 'untarget':
+                        labels.apply_(lambda x: (x + 1) % 10)
+                    else:
+                        labels.apply_(lambda x: 7)
                 inputs = Variable(inputs).to(device)
                 labels = Variable(labels).to(device)
                 loss += self.model.optimize_model(input_batch=inputs,
@@ -52,24 +57,6 @@ class Client():
                     self.model.exp_lr_sheduler(epoch=self.epoch)
                     # self.model.print_current_lr()
                     break
-            # layers = []
-            # for name, layer in self.model.shared_layers.name_layer.items():
-            #     if name == 'conv2_drop':
-            #         continue
-            #     layers.append(layer.weight.grad.flatten())
-            #     layers.append(layer.bias.grad.flatten())
-            # layers = torch.cat(layers).to(self.device)
-            # if torch.linalg.norm(layers) > 1:
-            #     layers = layers / torch.linalg.norm(layers)
-            if self.dataset == 'mnist':
-                layers = self.model.shared_layers.fc2.weight.grad.flatten()
-            else:
-                layers = self.model.shared_layers.linear.weight.grad.flatten()
-
-            layers = layers / torch.linalg.norm(layers)
-            self.grad_history = torch.add(self.grad_history, layers)
-
-
             if end: break
             self.epoch += 1
             self.model.exp_lr_sheduler(epoch = self.epoch)
@@ -77,6 +64,13 @@ class Client():
         # print(itered_num)
         # print(f'The {self.epoch}')
         loss /= num_iter
+        if self.training_model == 'lenet':
+            layers = self.model.shared_layers.fc2.weight.grad.flatten()
+        elif self.training_model == 'cnn_complex':
+            layers = self.model.shared_layers.fc_layer[-1].weight.grad.flatten()
+        elif self.training_model == 'resnet18':
+            layers = self.model.shared_layers.linear.weight.grad.flatten()
+        self.grad_history = layers / torch.linalg.norm(layers)
         return loss
 
     def test_model(self, device):
@@ -114,3 +108,19 @@ class Client():
         self.model.update_model(self.receiver_buffer)
         return None
 
+    def comit(self):
+        return torch.dot(self.s_prime, self.grad_history) / torch.norm(self.grad_history), torch.norm(self.grad_history)
+    def send_to_cloud(self, cloud):
+        message = {
+            'client_id': self.id,
+            'comit': self.comit(),
+        }
+        cloud.receive_from_client(message)
+        return
+    def receive_from_cloud(self, message):
+        if 'receipt' not in message:
+            self.s_prime = message['s_prime']
+            self.private_key = message['private_key']
+        else:
+            self.receipt = message['receipt']
+        return None
